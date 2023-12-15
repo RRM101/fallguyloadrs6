441d83389bb0b2863c242624510b65e99e383344
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2023-12-15 16:20:16 +0530

    more setting up repo

--- fallguyloadrs6/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace fallguyloadrold
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "fallguyloadrold";
-        public const string PLUGIN_NAME = "fallguyloadrold";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
